164f0a8f74723d66429fa8d77fcf451e03d262f7
ShimiraOleg <lopes3137@gmail.com>
2026-03-23 10:58:38 -0300

    commit api.py

--- trabalho_1/api.py
+++ trabalho_1/api.py
@@ -1,1 +1,23 @@
+import requests
+import json
+
+response = requests.get("https://db.ygoprodeck.com/api/v7/cardinfo.php?name=Dark%20Magician")
+
+print(response.status_code)
+
+if response.status_code == 200:
+    data = response.json()
+    try:
+        with open("data.json", "r") as json_file:
+            existing_data = json.load(json_file)
+    except (FileNotFoundError, json.decoder.JSONDecodeError):
+        existing_data = []
+
+    existing_data.append(data)
+
+    with open("data.json", "w") as json_file:
+        json.dump(existing_data, json_file, indent=4)
+else:
+    print("ERRO: ", response.status_code)
+
 #dae parmeiras
